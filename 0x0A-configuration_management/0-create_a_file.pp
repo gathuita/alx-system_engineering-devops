30a3af44bdf8762d987ca4ec8549cc11547bb505
gathuita <gathuitapius@gmail.com>
2022-10-15 02:26:33 -0700

    fix create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Using puppet, create a file with below specifications
-
-file { '/tmp/holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+# Creates a file school in /tmp with thr content 'I love Puppet'
+file { 'school':
+    ensure  => present,
+    path    => '/tmp/school',
+    content => 'I love Puppet',
+    group   => www-data,
+    owner   => www-data,
+    mode    => '0744'
 }
